3d63fbca2e03e001755ada08b603c2469fbe7807
pawor <pawor.emi@gmail.com>
2022-02-13 20:53:51 +0700

    setup

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/navbar/Navbar";
+import Featured from "../components/featured/Featured";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <Featured />
     </div>
   );
 };
